20e411a020b3d70794f547b87c5802e7e3493210
Prateek Sujaina <prateek.sujaina@labs108.com>
2017-08-04 16:23:00 +0530

    Initial Commit

---
+++ CreatePod/CreatePod.h
@@ -0,0 +1,19 @@
+//
+//  CreatePod.h
+//  CreatePod
+//
+//  Created by Prateek Sujaina on 04/08/17.
+//  Copyright © 2017 prateek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CreatePod.
+FOUNDATION_EXPORT double CreatePodVersionNumber;
+
+//! Project version string for CreatePod.
+FOUNDATION_EXPORT const unsigned char CreatePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CreatePod/PublicHeader.h>
+
+
